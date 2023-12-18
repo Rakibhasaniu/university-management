d12c9be09724707381244c7f86b0ad805a243d67
Rakibhsaniu <rakibhasanpypniu@gmail.com>
2023-12-18 21:14:45 +0600

    student getting successfully

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -16,7 +16,20 @@ const createStudent = async (req: Request, res: Response) => {
   }
 }
 
+const getAllStudent = async (req: Request, res: Response) => {
+  try{
+    const result = await StudentService.getAllStudentFromDB()
+    res.status(200).json({
+      success: true,
+      message: 'Student Getting Successfully',
+      dada: result
+    })
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 export const StudentController = {
   createStudent,
-  
+  getAllStudent,
 }
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,13 +1,18 @@
 import { TStudent } from "./student.interface";
 import { StudentModel } from "./student.model";
 
+
 const createStudentIntoDb = async (student: TStudent) => {
   const result =   await StudentModel.create(student)
+  return result;
+}
+const getAllStudentFromDB = async() => {
+  const result = await StudentModel.find();
   return result;
 }
 
 
 export const StudentService = {
     createStudentIntoDb,
-    
+    getAllStudentFromDB,
 }
