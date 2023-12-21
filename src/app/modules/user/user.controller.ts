9b9c5f311316bbb1ea753ef7e073a0bfa39ad498
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-12-21 21:59:55 +0600

    change

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -6,16 +6,16 @@ const createStudent = async (req: Request, res: Response) => {
   
       const zodParseData = studentValidationSchema.parse(studentData)
     const result = await UserServices.createStudentIntoDb(zodParseData)
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Student Created Successfully',
-    //   data: result
-    // })
-    // } catch (err: any){
-    //   res.status(500).json({
-    //     success: false,
-    //     message:err.message || 'Something Went Wrong',
-    //     error: err,
-    //   })
-    // }
+    res.status(200).json({
+      success: true,
+      message: 'Student Created Successfully',
+      data: result
+    })
+    } catch (err: any){
+      res.status(500).json({
+        success: false,
+        message:err.message || 'Something Went Wrong',
+        error: err,
+      })
+    }
   }
