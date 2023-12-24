6d9027784285b802d5be9eade533593a5d31dd27
Rakib Hasan <rakibhasanoyoniu@gmail.com>
2023-12-24 19:09:46 +0600

    validation solved

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -6,10 +6,10 @@ import { z } from "zod";
     password:z.string({
         invalid_type_error:'Password Must Be Strong'
     }).max(20,{message: 'Password can not be more than 20 characters'}).optional(),
-    // needsPasswordChange:z.boolean().optional().default(true),
-    // role:z.enum(['student','faculty','admin']),
-    // status:z.enum(['in-progress','blocked']).default('in-progress'),
-    // isDeleted: z.boolean().optional().default(false),
+    needsPasswordChange:z.boolean().optional().default(true),
+    role:z.enum(['student','faculty','admin']),
+    status:z.enum(['in-progress','blocked']).default('in-progress'),
+    isDeleted: z.boolean().optional().default(false),
 
 })
 
