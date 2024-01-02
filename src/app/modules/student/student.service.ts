0809d53427e08d9812ff77286c5f2c196155e7ff
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-01-03 01:44:00 +0600

    using transaction deleted student

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,5 +1,9 @@
+import mongoose from "mongoose";
 import { TStudent } from "./student.interface";
 import { StudentModel } from "./student.model";
+import AppError from "../../error/AppError";
+import { User } from "../user/user.model";
+import httpStatus from "http-status";
 
 
 const createStudentIntoDb = async (student: TStudent) => {
@@ -26,8 +30,26 @@ const getSingleStudent = async(id: string) => {
   return result;
 }
 const deleteStudent = async(id: string) => {
-  const result = await StudentModel.updateOne({id},{isDeleted: true})
-  return result;
+  const session = await mongoose.startSession()
+
+  try{
+    session.startTransaction()
+    const deletedStudent = await StudentModel.findOneAndUpdate({id},{isDeleted: true},{new: true ,session})
+    if(!deletedStudent){
+      throw new AppError(httpStatus.BAD_REQUEST,"Failed to delete student")
+    }
+    const deleteUser = await User.findOneAndUpdate({id},{isDeleted: true},{new: true ,session})
+    if(!deleteUser){
+      throw new AppError(httpStatus.BAD_REQUEST,'Failed to delete user')
+    }
+
+    await session.commitTransaction()
+    await session.endSession()
+    return deleteStudent;
+  } catch (err){
+    await session.abortTransaction()
+    await session.endSession()
+  }
 }
 
 
