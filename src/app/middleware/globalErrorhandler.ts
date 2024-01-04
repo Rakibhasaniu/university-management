314b8da3d2d900799c5982ac52df5edf4f44abb3
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-01-04 08:05:37 +0600

    error .ts created

--- src/app/middleware/globalErrorhandler.ts
+++ src/app/middleware/globalErrorhandler.ts
@@ -1,17 +1,56 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler } from "express";
+import { TErrorSources } from "../interface/error";
+import { ZodError, ZodIssue } from "zod";
 
 
 
 // eslint-disable-next-line no-unused-vars
-const globalErrorHandler = (err: any, req: Request, res: Response,next: NextFunction) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Something Went Wrong';
+const globalErrorHandler:ErrorRequestHandler = (err, req, res,next) => {
+    let statusCode = 500;
+    let message =  'Something Went Wrong';
+
+  let errorSources: TErrorSources = [
+    {
+      path:'',
+      message:'Something Went Wrong',
+    },
+  ]
+   const handleZodError = (err: ZodError) => {
+    const errorSources: TErrorSources = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue?.path[issue.path.length -1],
+        message: issue.message
+      }
+    } )
+    const statusCode = 400;
+
+
+    return {
+      statusCode,
+      message:'Zod Validation Error',
+      errorSources 
+    }
+
+
+   }
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  }
+
+
+
+
+
     return res.status(statusCode).json({
       success : false,
       message,
-      error: err,
+      errorSources,
+      // error: err,
     })
   }
 
