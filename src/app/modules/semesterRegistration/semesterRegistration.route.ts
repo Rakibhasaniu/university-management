086294042d77f5a576327830d81a2c3bc14cb0a0
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-01-08 22:00:34 +0600

    semesterRegistration interface and model created

---
+++ src/app/modules/semesterRegistration/semesterRegistration.route.ts
@@ -0,0 +1,41 @@
+// import express from 'express';
+// import { SemesterRegistrationController } from './semesterRegistration.controller';
+// import { SemesterRegistrationValidations } from './semesterRegistration.validation';
+// import validationMiddleware from '../../middleware/validateRequest';
+
+// const router = express.Router();
+
+// router.post(
+//   '/create-semester-registration',
+//   validationMiddleware(
+//     SemesterRegistrationValidations.createSemesterRegistrationValidationSchema,
+//   ),
+//   SemesterRegistrationController.createSemesterRegistration,
+// );
+
+// router.get(
+//   '/:id',
+//   SemesterRegistrationController.getSingleSemesterRegistration,
+// );
+
+// router.patch(
+//   '/:id',
+//   validationMiddleware(
+//     SemesterRegistrationValidations.upadateSemesterRegistrationValidationSchema,
+//   ),
+//   SemesterRegistrationController.updateSemesterRegistration,
+// );
+
+// router.get(
+//   '/:id',
+//   SemesterRegistrationController.getSingleSemesterRegistration,
+// );
+
+// router.delete(
+//   '/:id',
+//   SemesterRegistrationController.deleteSemesterRegistration,
+// );
+
+// router.get('/', SemesterRegistrationController.getAllSemesterRegistrations);
+
+// export const semesterRegistrationRoutes = router;
